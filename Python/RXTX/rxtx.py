4bf61aa8276b4f30811484de31b773875aecd37d
Hinrich Kaestner <hinrich.kaestner@outlook.com>
2019-09-09 01:45:08 +0200

    Sent error message on failure to send

--- Python/RXTX/rxtx.py
+++ Python/RXTX/rxtx.py
@@ -89,10 +89,10 @@ class RXTX(object):
                         # send GET request
                         # u.sendAT('AT+UHTTPC=0,1,"{}","get.ffs"\r\n'.format(url))
                         # send POST request with data in application/json form
-                        if u.sendAT('AT+UHTTPC=0,5,"{}","post.ffs","{}",4\r\n'.format(url, message), "UUHTTPCR\r\n"):
-                            print("UUHTTPCR handler")
+                        if u.sendAT('AT+UHTTPC=0,5,"{}","post.ffs","{}",4\r\n'.format(url, message), "UUHTTPCR: 0,5,1\r\n"):
+                            ser.write(b'Sent successfully')
                         else:
-                            print("some other handler")
+                            ser.write(b'Error occured while sending')
                     except ValueError:
                         print(message)
             except Exception, e:
